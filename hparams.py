fb6da6b18b3efcbcc843d423c396ec9f5c341f78
Partha Pratim Deka <parthapratimdeka129@gmail.com>
2025-03-21 15:44:11 +0530

    update: arguments, functions to cover up for newer versions

--- hparams.py
+++ hparams.py
@@ -73,7 +73,7 @@ class HParams:
         learning_rate=1e-3
         weight_decay=1e-6
         grad_clip_thresh=1.0
-        batch_size=64
+        batch_size=32
         mask_padding=True  # set model's padded outputs to padded value
     
 
